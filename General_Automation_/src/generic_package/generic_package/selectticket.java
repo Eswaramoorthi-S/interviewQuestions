635b26eb813d5dca08cbd8b92b97f276fcc422f7
Dell <Dell@DESKTOP-ILOVP9M>
2023-11-28 14:15:50 +0530

    secound commit

---
+++ General_Automation_/src/generic_package/generic_package/selectticket.java
@@ -0,0 +1,29 @@
+package generic_package.generic_package;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class selectticket {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://blazedemo.com/index.php");
+		driver.manage().window().maximize();
+		WebElement ele = driver.findElement(By.xpath("//select[@class='form-inline' and@name='fromPort']"));
+		Select s=new Select(ele);
+		s.selectByValue("Mexico City");
+		WebElement ele1 = driver.findElement(By.xpath("//select[@class='form-inline' and@name='toPort']"));
+		Select s1=new Select(ele1);
+		s1.selectByValue("London");
+		driver.findElement(By.xpath("//input[@class='btn btn-primary']")).click();
+		driver.findElement(By.xpath("(//input[@class='btn btn-small'and @type='submit' ])[3]")).click();
+		driver.findElement(By.xpath("//input[@class='btn btn-primary']")).click();
+		String id = driver.findElement(By.xpath("(//td['Id']/../td[2])[1]")).getText();
+		System.out.println("the ticket id is="+id);
+		driver.close();
+	}
+
+}
---
+++ General_Automation_/src/generic_package/generic_package/tocheckthetitle.java
@@ -0,0 +1,26 @@
+package generic_package.generic_package;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class tocheckthetitle {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+	    driver.get("https://blazedemo.com/index.php");	
+	    WebElement ele = driver.findElement(By.xpath("//a[@href='vacation.html']"));
+	    boolean res=ele.isDisplayed();
+	    		if(res) {
+	    			System.out.println("title is displayed");
+	    		}
+	    		else
+	    		{
+	    			System.out.println("title is not displayed");
+	    		}
+	    		driver.close();
+	     
+	    
+	    
+	}}
